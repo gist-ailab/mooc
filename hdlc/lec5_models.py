8445fdd03f5b803803bcd49b68ef2c7276f679e2
seunghyeok back <bsh3681024@gist.ac.kr>
2019-10-17 01:21:57 +0900

    lec5_final

--- hdlc/lec5_models.py
+++ hdlc/lec5_models.py
@@ -11,7 +11,7 @@ class ConvNet(nn.Module):
         self.layer2 = nn.Sequential(nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
                                     nn.ReLU(),
                                     nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(7 * 7 * 32, num_classes)
+        self.fc = nn.Linear(8 * 8 * 32, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
@@ -21,9 +21,7 @@ class ConvNet(nn.Module):
         prob = F.softmax(score, dim=1)
         return prob
 
-
 class AlexNet(nn.Module):
-
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
@@ -41,12 +39,9 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
@@ -54,17 +49,14 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
-
 
 class VGGNet(nn.Module):
     def __init__(self, num_classes=10):
         super(VGGNet, self).__init__()
         self.features = nn.Sequential(
-            # convolutional layers
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
@@ -101,15 +93,26 @@ class VGGNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
+
         self.classifier = nn.Sequential(
             nn.Linear(7 * 7 * 512, 4096),
             nn.ReLU(),
-            nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(),
-            nn.Dropout(),
             nn.Linear(4096, num_classes)
         )
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = self.features(x)
